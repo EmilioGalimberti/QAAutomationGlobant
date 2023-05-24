92d8926b79ae4a59e8a82c1510d02b6af31b96f6
EmilioGalimberti <emiliogalimberty@gmail.com>
2023-05-23 21:00:27 -0300

    Tema helloWorld

--- learningJava/clase1/introJava/src/introJavaPackage/Main.java
+++ learningJava/clase1/introJava/src/introJavaPackage/Main.java
@@ -1,6 +1,7 @@
 package introJavaPackage;
-
 import java.util.Scanner;
+
+//Tema helloWorld
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
