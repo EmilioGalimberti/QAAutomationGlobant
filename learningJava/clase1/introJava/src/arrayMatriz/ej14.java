880c7d078c9c0b3ba76b180bc0fe0edc524d9167
EmilioGalimberti <emiliogalimberty@gmail.com>
2023-06-06 19:32:48 -0300

    arrays

---
+++ learningJava/clase1/introJava/src/arrayMatriz/ej14.java
@@ -0,0 +1,18 @@
+package arrayMatriz;
+import java.util.Scanner;
+
+public class ej14 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("ingrese la canitdad que desa convertir: ");
+        int euro = scanner.nextInt();
+
+        double[] valoresMonedas = {0.86,1.28,129.85};
+        String[] nombreCambio = {"libra","Dolar","Yenes"};
+
+        for (int i = 0; i < valoresMonedas.length; i++){
+            double cambio = valoresMonedas[i] * euro;
+            System.out.println(cambio+ " " + nombreCambio[i]);
+        }
+    }
+}
