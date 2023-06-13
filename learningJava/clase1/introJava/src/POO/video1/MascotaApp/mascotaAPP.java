7d96fa496afbd0fca7c4fccaa292b2828af494ce
EmilioGalimberti <emiliogalimberty@gmail.com>
2023-06-13 20:51:36 -0300

    POO v1

---
+++ learningJava/clase1/introJava/src/POO/video1/MascotaApp/mascotaAPP.java
@@ -0,0 +1,9 @@
+package POO.video1.MascotaApp;
+
+import POO.video1.MascotaEntidades.Perro;
+
+public class mascotaAPP {
+    public static void main(String[] args) {
+        Perro perro1 = new Perro();
+    }
+}
---
+++ learningJava/clase1/introJava/src/POO/video1/MascotaEntidades/Perro.java
@@ -0,0 +1,8 @@
+package POO.video1.MascotaEntidades;
+
+public class Perro {
+    public String nombre;
+    public String raza;
+    public String apodo;
+    public int edad;
+}
