a9bc5f446a5cdf48ef034f96865c8d7800f53e1e
EmilioGalimberti <emiliogalimberty@gmail.com>
2023-06-13 21:16:00 -0300

    proyectoAppMascota v2

---
+++ learningJava/proyectoJava1/src/masctoApp/entidades/Mascota.java
@@ -0,0 +1,11 @@
+package masctoApp.entidades;
+
+public class Mascota {
+    public String nombre;
+    public String apodo;
+    public String tipo;
+    public String color;
+    public int edad;
+    public boolean cola;
+    public String raza;
+}
---
+++ learningJava/proyectoJava1/src/masctoApp/entidades/User.java
@@ -0,0 +1,11 @@
+package masctoApp.entidades;
+
+import java.util.Date;
+
+public class User {
+    public String nombre;
+    public String apellido;
+    public int dni;
+    public Date nacimiento;
+    public String pais;
+}
