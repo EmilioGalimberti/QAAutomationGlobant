3c74b582cab2164fc9f42dc88777e05b8ceecdae
EmilioGalimberti <emiliogalimberty@gmail.com>
2023-06-15 20:46:58 -0300

    Encapsulamiento getter v6

--- learningJava/proyectoJava1/src/MascotaAPP.java
+++ learningJava/proyectoJava1/src/MascotaAPP.java
@@ -5,13 +5,12 @@ import java.util.Scanner;
 public class MascotaAPP {
     public static void main(String[] args) {
         Scanner leer = new Scanner(System.in);
-        Mascota mascota1 = new Mascota("Fernando chiquito",leer.nextLine(),"perro");
-
-        System.out.println(mascota1);
+        Mascota mascota1 = new Mascota("Fernando chiquito", "pepe", "perro");
 
         //si los atributos fueran publicos
         //mascota1.nombre = "aka";
         mascota1.setNombre("pepe");
+        System.out.println(mascota1.toString());
 
     }
 }
