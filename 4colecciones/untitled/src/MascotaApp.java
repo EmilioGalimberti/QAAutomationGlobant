47177813e642ca1b8d0271f4b5d9b3797c1b7f8f
EmilioGalimberti <emiliogalimberty@gmail.com>
2023-07-25 19:51:53 -0300

    V3 Colecciones

--- 4colecciones/untitled/src/MascotaApp.java
+++ 4colecciones/untitled/src/MascotaApp.java
@@ -7,27 +7,32 @@ public class MascotaApp {
 
     public static void main(String[] args){
         //Arrays
-        String[] nombreArray = new String[5];
-
-        for (int i=0 ; i < nombreArray.length; i++){
-            nombreArray[i]= "Chiquito "+(i+1);
-        }
-
-        for (String var : nombreArray){
-            System.out.println(var);
-        }
+//        String[] nombreArray = new String[5];
+//
+//        for (int i=0 ; i < nombreArray.length; i++){
+//            nombreArray[i]= "Chiquito "+(i+1);
+//        }
+//
+//        for (String var : nombreArray){
+//            System.out.println(var);
+//        }
 
         //Collections
-        ArrayList<String> nombreArrayList = new ArrayList<>();
-        System.out.println(nombreArrayList.size());
-        nombreArrayList.add("Chiquito");
-        System.out.println(nombreArrayList.size());
-        nombreArrayList.remove("Chiquito");
-        System.out.println(nombreArrayList.size());
+//        ArrayList<String> nombreArrayList = new ArrayList<>();
+//        System.out.println(nombreArrayList.size());
+//        nombreArrayList.add("Chiquito");
+//        System.out.println(nombreArrayList.size());
+//        nombreArrayList.remove("Chiquito");
+//        System.out.println(nombreArrayList.size());
 
         ServiceMascota serviceMascota = new ServiceMascota();
-        serviceMascota.crearMascota();
-        serviceMascota.crearMascota();
+//        serviceMascota.crearMascota();
+//        serviceMascota.crearMascota();
+//        serviceMascota.mostrarMascotas();
+
+        serviceMascota.fabricaChiquitos(140);
+        serviceMascota.mostrarMascotas();
+        serviceMascota.fabricaMascotas(2);
         serviceMascota.mostrarMascotas();
     }
 }
